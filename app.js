cd6e79c0c1cb98f78b87d9a0d17e50dc70f0c636
Nikhil Anand <nikhilanand1006@gmail.com>
2019-09-17 02:04:44 +0530

    Ongoing

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const path = require('path');
+const db = require("./db");
+const collection = "todo";
+
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req, res) => {
+    db.getDB().collection(collection).find({}).toArray((err, documents) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(documents);
+        }
+    });
+});
+
+app.post('/getActorProfile', (req, res) => {
+    db.getDB().collection("actors").find({search : req.body.name}).toArray((err, document) => {
+        if (err) {
+            console.log(err);
+        } else {
+            return res.json(document[0]);
+        }
+    })
+});
+
+app.put('/:id', (req, res) => {
+    const todoID = req.params.id;
+    const userInput = req.body;
+    db.getDB().collection(collection).findOneAndUpdate({_id: db.getPrimaryKey(todoID)}, {$set: {todo: userInput.todo}}, {returnOriginal: false}, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    })
+});
+
+app.post('/', (req, res) => {
+    const userInput = req.body;
+    db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json({result: result, document: result.ops[0]});
+        }
+    })
+});
+
+app.delete('/:id', (req, res) => {
+    const todoID = req.params.id;
+    db.getDB().collection(collection).findOneAndDelete({_id: db.getPrimaryKey(todoID)}, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    })
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("Unable to connect");
+        process.exit(1);
+    } else {
+        app.listen(27017, () => {
+            console.log("Connected | App listening");
+        });
+    }
+});
+
+
+// getting pages
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/login.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/signup.html'));
+});
+
+app.get('/actorProfile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/actorProfile.html'));
+});
---
+++ db.js
@@ -0,0 +1,34 @@
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const dbName = "myIMDB";
+const url = "mongodb://localhost:27017";
+const  mongoOptions = {useUnifiedTopology : true, useNewUrlParser : true};
+
+const state = {
+    db : null
+};
+
+const connect = (cb) =>  {
+    if(state.db) {
+        cb();
+    } else {
+        MongoClient.connect(url, mongoOptions, (err, client) => {
+            if (err) {
+                cb(err);
+            } else {
+                state.db = client.db(dbName);
+                cb();
+            }
+        });
+    }
+};
+
+const getPrimaryKey = (_id) => {
+    return ObjectID(_id);
+};
+
+const getDB = () => {
+    return state.db;
+};
+
+module.exports = {getDB, connect, getPrimaryKey};
